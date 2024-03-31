f9d1ee1e61c53ca70826ee41a6c7390dd21c481d
devender9695 <devenderkumar9695@gmail.com>
2024-03-31 23:42:22 +0530

    project done

---
+++ config/db.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+
+const connectDb = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log(`Connected To Database `);
+  } catch (error) {
+    console.log("DB Error", error);
+  }
+};
+module.exports = connectDb;
